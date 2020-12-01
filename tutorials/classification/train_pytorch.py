1446e4c289206d8bd5c844ad79621612c8a4461a
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2020-12-01 22:04:42 +0000

    ref #2 adding wip class-pytorch

--- tutorials/classification/train_pytorch.py
+++ tutorials/classification/train_pytorch.py
@@ -1,65 +1,39 @@
-# -*- coding: utf-8 -*-
-import random
 import torch
 
 
-class DynamicNet(torch.nn.Module):
-    def __init__(self, D_in, H, D_out):
-        """
-        In the constructor we construct three nn.Linear instances that we will use
-        in the forward pass.
-        """
-        super(DynamicNet, self).__init__()
-        self.input_linear = torch.nn.Linear(D_in, H)
-        self.middle_linear = torch.nn.Linear(H, H)
-        self.output_linear = torch.nn.Linear(H, D_out)
+# define a vgg-16 net
+class VGG(torch.nn.Module, ch_in, ch_out):
+    def __init__(self):
+        super(VGG, self).__init__()
+        self.input_layer = conv2d_block(64, ch_in)
+        self.output_layer = torch.nn.Linear(512, ch_out)
 
     def forward(self, x):
-        """
-        For the forward pass of the model, we randomly choose either 0, 1, 2, or 3
-        and reuse the middle_linear Module that many times to compute hidden layer
-        representations.
+        cfg = [64, 64, 128, 128, 256, 256, 256, 512, 512, 512, 512, 512, 512]
+        '''
+        'VGG11': [64, 128, 256, 256, 512, 512, 512, 512],
+        'VGG13': [64, 64, 128, 128, 256, 256, 512, 512, 512, 512],
+        'VGG16': [64, 64, 128, 128, 256, 256, 256, 512, 512, 512, 512, 512, 512],
+        'VGG19': [64, 64, 128, 128, 256, 256, 256, 256, 512, 512, 512, 512, 512, 512, 512, 512],
+        '''
+        layers = self.input_layer(x)
+        for idx,x in enumerate(cfg):
+            layers += [conv2d_block()]
+        layers += [torch.nn.AvgPool2d(1)]
 
-        Since each forward pass builds a dynamic computation graph, we can use normal
-        Python control-flow operators like loops or conditional statements when
-        defining the forward pass of the model.
+        return self.output_layer(torch.nn.Sequential(*layers))
 
-        Here we also see that it is perfectly safe to reuse the same Module many
-        times when defining a computational graph. This is a big improvement from Lua
-        Torch, where each Module could be used only once.
-        """
-        h_relu = self.input_linear(x).clamp(min=0)
-        for _ in range(random.randint(0, 3)):
-            h_relu = self.middle_linear(h_relu).clamp(min=0)
-        y_pred = self.output_linear(h_relu)
-        return y_pred
+    def conv2d_block(ch_out, ch_in, post_pooling=False):
+        block = [torch.nn.Conv2d(ch_in,ch_out,3), torch.nn.ReLU(), torch.nn.BatchNorm2d()]
+        if post_pooling:
+            block += [torch.nn.MaxPool2d(kernel_size=2, stride=2)]
+        return block 
 
 
-# N is batch size; D_in is input dimension;
-# H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 64, 1000, 100, 10
 
-# Create random Tensors to hold inputs and outputs
-x = torch.randn(N, D_in)
-y = torch.randn(N, D_out)
 
-# Construct our model by instantiating the class defined above
-model = DynamicNet(D_in, H, D_out)
-
-# Construct our loss function and an Optimizer. Training this strange model with
-# vanilla stochastic gradient descent is tough, so we use momentum
-criterion = torch.nn.MSELoss(reduction='sum')
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-4, momentum=0.9)
-for t in range(500):
-    # Forward pass: Compute predicted y by passing x to the model
-    y_pred = model(x)
-
-    # Compute and print loss
-    loss = criterion(y_pred, y)
-    if t % 100 == 99:
-        print(t, loss.item())
-
-    # Zero gradients, perform a backward pass, and update the weights.
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+        def test():
+    net = VGG('VGG11')
+    x = torch.randn(2,3,32,32)
+    y = net(x)
+    print(y.size())
