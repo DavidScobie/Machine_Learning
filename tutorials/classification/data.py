84bc40d71e3fd26f7441e319517d1445d501a292
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2020-12-07 22:37:17 +0000

    ref #1 remove tf dependency in data.py

--- tutorials/classification/data.py
+++ tutorials/classification/data.py
@@ -1,19 +1,28 @@
 
 import os
-from tensorflow.keras.utils import get_file
+# from tensorflow.keras.utils import get_file
 # from torchvision.datasets.utils import download_and_extract_archive
+import requests
 
 # specify two useful folders
 DATA_PATH = './data'
 RESULT_PATH = './result'
 
-
-# download the data first
 if not os.path.exists(DATA_PATH):
     os.makedirs(DATA_PATH)
+
+# download the data first
+'''
 filename = get_file(fname=os.path.abspath(os.path.join(DATA_PATH,'ultrasound_50frames.h5')),
                     origin='https://weisslab.cs.ucl.ac.uk/WEISSTeaching/datasets/raw/fetal/ultrasound_50frames.h5')
+'''
 
+print('Downloading and extracting data...')
+url = 'https://weisslab.cs.ucl.ac.uk/WEISSTeaching/datasets/raw/fetal/ultrasound_50frames.h5' 
+r = requests.get(url,allow_redirects=True)
+filename = os.path.join(DATA_PATH,'ultrasound_50frames.h5')
+_ = open(filename,'wb').write(r.content)
+print('Done.')
 
 if not os.path.exists(RESULT_PATH):
     os.makedirs(RESULT_PATH)
--- tutorials/segmentation/data.py
+++ tutorials/segmentation/data.py
@@ -1,8 +1,7 @@
 
 import os
-from tensorflow.keras.utils import get_file
 import zipfile
-
+import requests
 
 '''
 # download data
@@ -16,13 +15,26 @@ os.system('mkdir result')
 DATA_PATH = './data'
 RESULT_PATH = './result'
 
-os.makedirs(DATA_PATH)
+if not os.path.exists(DATA_PATH):
+    os.makedirs(DATA_PATH)
+
+'''
+from tensorflow.keras.utils import get_file
 temp_file = get_file(fname='datasets-promise12.zip',
                      origin='https://weisslab.cs.ucl.ac.uk/WEISSTeaching/datasets/-/archive/promise12/datasets-promise12.zip')
+'''
+
+print('Downloading and extracting data...')
+url = 'https://weisslab.cs.ucl.ac.uk/WEISSTeaching/datasets/-/archive/promise12/datasets-promise12.zip' 
+r = requests.get(url,allow_redirects=True)
+temp_file = 'temp.zip'
+_ = open(temp_file,'wb').write(r.content)
+
 with zipfile.ZipFile(temp_file,'r') as zip_obj:
     zip_obj.extractall(DATA_PATH)
 os.remove(temp_file)
 os.makedirs(RESULT_PATH)
+print('Done.')
 
 print('Promise12 data downloaded: <%s>.' % os.path.abspath(os.path.join(DATA_PATH,'datasets-promise12')))
 print('Result directory created: <%s>.' % os.path.abspath(RESULT_PATH))
