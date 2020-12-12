044e3fef6d2626e0869817e815506e74967bd840
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2020-12-12 02:09:51 +0000

    ref #3 wip data loader added

--- tutorials/segmentation/train_pt.py
+++ tutorials/segmentation/train_pt.py
@@ -1,46 +1,38 @@
-from collections import OrderedDict
-
 import torch
-import torch.nn as nn
+import numpy as np
 
+folder_name = './data/datasets-promise12'
 
-class UNet(nn.Module):
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")
 
-    def __init__(self, in_channels=3, out_channels=1, init_features=32):
+class UNet(torch.nn.Module):
+
+    def __init__(self, ch_in=1, ch_out=1, init_n_feat=32):
         super(UNet, self).__init__()
 
-        features = init_features
-        self.encoder1 = UNet._block(in_channels, features, name="enc1")
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder2 = UNet._block(features, features * 2, name="enc2")
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder3 = UNet._block(features * 2, features * 4, name="enc3")
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder4 = UNet._block(features * 4, features * 8, name="enc4")
-        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+        n_feat = init_n_feat
+        self.encoder1 = UNet._block(ch_in, n_feat)
+        self.pool1 = torch.nn.MaxPool3d(kernel_size=2, stride=2)
+        self.encoder2 = UNet._block(n_feat, n_feat*2)
+        self.pool2 = torch.nn.MaxPool3d(kernel_size=2, stride=2)
+        self.encoder3 = UNet._block(n_feat*2, n_feat*4)
+        self.pool3 = torch.nn.MaxPool3d(kernel_size=2, stride=2)
+        self.encoder4 = UNet._block(n_feat*4, n_feat*8)
+        self.pool4 = torch.nn.MaxPool3d(kernel_size=2, stride=2)
 
-        self.bottleneck = UNet._block(features * 8, features * 16, name="bottleneck")
+        self.bottleneck = UNet._block(n_feat*8, n_feat*16)
 
-        self.upconv4 = nn.ConvTranspose2d(
-            features * 16, features * 8, kernel_size=2, stride=2
-        )
-        self.decoder4 = UNet._block((features * 8) * 2, features * 8, name="dec4")
-        self.upconv3 = nn.ConvTranspose2d(
-            features * 8, features * 4, kernel_size=2, stride=2
-        )
-        self.decoder3 = UNet._block((features * 4) * 2, features * 4, name="dec3")
-        self.upconv2 = nn.ConvTranspose2d(
-            features * 4, features * 2, kernel_size=2, stride=2
-        )
-        self.decoder2 = UNet._block((features * 2) * 2, features * 2, name="dec2")
-        self.upconv1 = nn.ConvTranspose2d(
-            features * 2, features, kernel_size=2, stride=2
-        )
-        self.decoder1 = UNet._block(features * 2, features, name="dec1")
+        self.upconv4 = torch.nn.ConvTranspose3d(n_feat*16, n_feat*8, kernel_size=3, stride=2)
+        self.decoder4 = UNet._block((n_feat*8)*2, n_feat*8)
+        self.upconv3 = torch.nn.ConvTranspose3d(n_feat*8, n_feat*4, kernel_size=3, stride=2)
+        self.decoder3 = UNet._block((n_feat*4)*2, n_feat*4)
+        self.upconv2 = torch.nn.ConvTranspose3d(n_feat*4, n_feat*2, kernel_size=3, stride=2)
+        self.decoder2 = UNet._block((n_feat*2)*2, n_feat*2)
+        self.upconv1 = torch.nn.ConvTranspose3d(n_feat*2, n_feat, kernel_size=3, stride=2)
+        self.decoder1 = UNet._block(n_feat*2, n_feat)
 
-        self.conv = nn.Conv2d(
-            in_channels=features, out_channels=out_channels, kernel_size=1
-        )
+        self.conv = torch.nn.Conv3d(in_channels=n_feat, out_channels=ch_out, kernel_size=1)
 
     def forward(self, x):
         enc1 = self.encoder1(x)
@@ -65,39 +57,17 @@ class UNet(nn.Module):
         return torch.sigmoid(self.conv(dec1))
 
     @staticmethod
-    def _block(in_channels, features, name):
-        return nn.Sequential(
-            OrderedDict(
-                [
-                    (
-                        name + "conv1",
-                        nn.Conv2d(
-                            in_channels=in_channels,
-                            out_channels=features,
-                            kernel_size=3,
-                            padding=1,
-                            bias=False,
-                        ),
-                    ),
-                    (name + "norm1", nn.BatchNorm2d(num_features=features)),
-                    (name + "relu1", nn.ReLU(inplace=True)),
-                    (
-                        name + "conv2",
-                        nn.Conv2d(
-                            in_channels=features,
-                            out_channels=features,
-                            kernel_size=3,
-                            padding=1,
-                            bias=False,
-                        ),
-                    ),
-                    (name + "norm2", nn.BatchNorm2d(num_features=features)),
-                    (name + "relu2", nn.ReLU(inplace=True)),
-                ]
-            )
-)
+    def _block(ch_in, n_feat):
+        return torch.nn.Sequential([
+                                    torch.nn.Conv3d(in_channels=ch_in, out_channels=n_feat, kernel_size=3, padding=1, bias=False),
+                                    torch.nn.BatchNorm3d(num_features=n_feat),
+                                    torch.nn.ReLU(inplace=True),
+                                    torch.nn.Conv3d(in_channels=n_feat, out_channels=n_feat, kernel_size=3, padding=1, bias=False),
+                                    torch.nn.BatchNorm3d(num_features=n_feat),
+                                    torch.nn.ReLU(inplace=True)
+                                    ])
 
-class DiceLoss(nn.Module):
+class DiceLoss(torch.nn.Module):
     def __init__(self):
         super(DiceLoss, self).__init__()
         self.smooth = 1.0
@@ -123,101 +93,57 @@ def binary_dice(y_true, y_pred):
         return numerator * 1.0 / denominator
 
 
-from collections import OrderedDict
+# now define a data loader
+class DatasetTrain(torch.utils.data.Dataset):
+    def __init__(self, folder_name):
+        self.folder_name = folder_name
+    
+    def __len__(self):
+        return self.num_subjects = 50
 
-import torch
-import torch.nn as nn
+    def __getitem__(self, index):
+        image = np.float32(np.load(os.path.join(self.folder_name, "image_train%02d.npy" % index)))
+        label = np.float32(np.load(os.path.join(self.folder_name, "label_train%02d.npy" % index)))       
+        return (image, label)
 
 
-class UNet(nn.Module):
+# image_test = np.float32(np.load(os.path.join(self.folder_name, "image_test%02d.npy" % 30)))     
 
-    def __init__(self, in_channels=3, out_channels=1, init_features=32):
-        super(UNet, self).__init__()
 
-        features = init_features
-        self.encoder1 = UNet._block(in_channels, features, name="enc1")
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder2 = UNet._block(features, features * 2, name="enc2")
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder3 = UNet._block(features * 2, features * 4, name="enc3")
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder4 = UNet._block(features * 4, features * 8, name="enc4")
-        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+# training
+model = VGGNet(1,4)
 
-        self.bottleneck = UNet._block(features * 8, features * 16, name="bottleneck")
+train_set = H5Dataset(filename)
+train_loader = torch.utils.data.DataLoader(
+    train_set,
+    batch_size=8, 
+    shuffle=True,
+    num_workers=8)
+'''
+dataiter = iter(train_loader)
+frames, labels = dataiter.next()
+'''
 
-        self.upconv4 = nn.ConvTranspose2d(
-            features * 16, features * 8, kernel_size=2, stride=2
-        )
-        self.decoder4 = UNet._block((features * 8) * 2, features * 8, name="dec4")
-        self.upconv3 = nn.ConvTranspose2d(
-            features * 8, features * 4, kernel_size=2, stride=2
-        )
-        self.decoder3 = UNet._block((features * 4) * 2, features * 4, name="dec3")
-        self.upconv2 = nn.ConvTranspose2d(
-            features * 4, features * 2, kernel_size=2, stride=2
-        )
-        self.decoder2 = UNet._block((features * 2) * 2, features * 2, name="dec2")
-        self.upconv1 = nn.ConvTranspose2d(
-            features * 2, features, kernel_size=2, stride=2
-        )
-        self.decoder1 = UNet._block(features * 2, features, name="dec1")
+criterion = torch.nn.CrossEntropyLoss(reduction='mean')
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-        self.conv = nn.Conv2d(
-            in_channels=features, out_channels=out_channels, kernel_size=1
-        )
+freq_print = 200
+for epoch in range(20):
+    for ii, data in enumerate(train_loader, 0):
+        
+        moving_loss = 0.0
+        frames, labels = data
 
-    def forward(self, x):
-        enc1 = self.encoder1(x)
-        enc2 = self.encoder2(self.pool1(enc1))
-        enc3 = self.encoder3(self.pool2(enc2))
-        enc4 = self.encoder4(self.pool3(enc3))
+        optimizer.zero_grad()
+        outputs = model(frames)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
 
-        bottleneck = self.bottleneck(self.pool4(enc4))
+        # Compute and print loss
+        moving_loss += loss.item()
+        if ii % freq_print == (freq_print-1):    # print every 2000 mini-batches
+            print('[Epoch %d, iter %05d] loss: %.3f' % (epoch, ii, moving_loss/freq_print))
+            moving_loss = 0.0
 
-        dec4 = self.upconv4(bottleneck)
-        dec4 = torch.cat((dec4, enc4), dim=1)
-        dec4 = self.decoder4(dec4)
-        dec3 = self.upconv3(dec4)
-        dec3 = torch.cat((dec3, enc3), dim=1)
-        dec3 = self.decoder3(dec3)
-        dec2 = self.upconv2(dec3)
-        dec2 = torch.cat((dec2, enc2), dim=1)
-        dec2 = self.decoder2(dec2)
-        dec1 = self.upconv1(dec2)
-        dec1 = torch.cat((dec1, enc1), dim=1)
-        dec1 = self.decoder1(dec1)
-        return torch.sigmoid(self.conv(dec1))
-
-    @staticmethod
-    def _block(in_channels, features, name):
-        return nn.Sequential(
-            OrderedDict(
-                [
-                    (
-                        name + "conv1",
-                        nn.Conv2d(
-                            in_channels=in_channels,
-                            out_channels=features,
-                            kernel_size=3,
-                            padding=1,
-                            bias=False,
-                        ),
-                    ),
-                    (name + "norm1", nn.BatchNorm2d(num_features=features)),
-                    (name + "relu1", nn.ReLU(inplace=True)),
-                    (
-                        name + "conv2",
-                        nn.Conv2d(
-                            in_channels=features,
-                            out_channels=features,
-                            kernel_size=3,
-                            padding=1,
-                            bias=False,
-                        ),
-                    ),
-                    (name + "norm2", nn.BatchNorm2d(num_features=features)),
-                    (name + "relu2", nn.ReLU(inplace=True)),
-                ]
-            )
-)
+print('Training done.')
