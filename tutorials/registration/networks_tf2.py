96dd52360c7132721460ab582b60aa148d940e31
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2020-12-27 21:14:56 +0000

    ref #6 initial files added

---
+++ tutorials/registration/networks_tf2.py
@@ -0,0 +1,156 @@
+# *** This code is with an Apache 2.0 license, University College London ***
+
+import tensorflow as tf
+
+
+def var_conv_kernel(ch_in, ch_out, name='W', initialiser=None):
+    with tf.variable_scope(name):
+        k_conv = [3, 3]
+        if initialiser is None:
+            initialiser = tf.contrib.layers.xavier_initializer()
+        return tf.get_variable(name, shape=k_conv+[ch_in]+[ch_out], initializer=initialiser)
+
+
+def conv_block(input_, ch_in, ch_out, strides=None, name='conv_block'):
+    if strides is None:
+        strides = [1, 1, 1, 1]
+    with tf.variable_scope(name):
+        w = var_conv_kernel(ch_in, ch_out)
+        return tf.nn.relu(tf.contrib.layers.batch_norm(tf.nn.conv2d(input_, w, strides, "SAME")))
+
+
+def deconv_block(input_, ch_in, ch_out, shape_out, strides, name='deconv_block'):
+    with tf.variable_scope(name):
+        w = var_conv_kernel(ch_out, ch_in)
+        return tf.nn.relu(tf.contrib.layers.batch_norm(tf.nn.conv2d_transpose(input_, w, shape_out, strides, "SAME")))
+
+
+def downsample_resnet_block(input_, ch_in, ch_out, name='down_resnet_block'):
+    strides1 = [1, 1, 1, 1]
+    strides2 = [1, 2, 2, 1]
+    k_pool = [1, 2, 2, 1]
+    with tf.variable_scope(name):
+        h0 = conv_block(input_, ch_in, ch_out, name='W0')
+        r1 = conv_block(h0, ch_out, ch_out, name='WR1')
+        wr2 = var_conv_kernel(ch_out, ch_out)
+        r2 = tf.nn.relu(tf.contrib.layers.batch_norm(tf.nn.conv2d(r1, wr2, strides1, "SAME")) + h0)
+        h1 = tf.nn.max_pool(r2, k_pool, strides2, padding="SAME")
+        return h1, h0
+
+
+def upsample_resnet_block(input_, input_skip, ch_in, ch_out, name='up_resnet_block'):
+    strides1 = [1, 1, 1, 1]
+    strides2 = [1, 2, 2, 1]
+    size_out = input_skip.shape.as_list()
+    with tf.variable_scope(name):
+        h0 = deconv_block(input_, ch_in, ch_out, size_out, strides2)
+        r1 = h0 + input_skip
+        r2 = conv_block(h0, ch_out, ch_out)
+        wr2 = var_conv_kernel(ch_out, ch_out)
+        h1 = tf.nn.relu(tf.contrib.layers.batch_norm(tf.nn.conv2d(r2, wr2, strides1, "SAME")) + r1)
+        return h1
+
+
+def ddf_output(input_, ch_in, name='ddf_summand'):
+    strides1 = [1, 1, 1, 1]
+    initial_std_local = 0.01
+    initial_bias_local = 0.01
+    with tf.variable_scope(name):
+        w = var_conv_kernel(ch_in, 2, initialiser=tf.random_normal_initializer(0, initial_std_local))
+        b = tf.get_variable(name, shape=[2], initializer=tf.constant_initializer(initial_bias_local))
+        return tf.nn.conv2d(input_, w, strides1, "SAME") + b
+
+
+def get_reference_grid(grid_size):
+    # grid_size: [batch_size, height, width]
+    grid = tf.cast(tf.stack(tf.meshgrid(
+                        tf.range(grid_size[1]),
+                        tf.range(grid_size[2]),
+                        indexing='ij'), axis=2), dtype=tf.float32)
+    return tf.tile(tf.expand_dims(grid, axis=0), [grid_size[0],1,1,1])
+
+
+def warp_grid(grid, transform):
+    # grid: [batch, height, width, 2]
+    # transform: [batch, 3, 3]
+    batch_size, height, width = grid.shape[0:3]
+    grid = tf.concat([tf.reshape(grid,[batch_size,height*width,2]), 
+                    tf.ones([batch_size,height*width,1])], axis=2)
+    grid_warped = tf.matmul(grid, transform)
+    return tf.reshape(grid_warped[...,:2], [batch_size,height,width,2])
+
+
+def resample_linear(grid_data, sample_grids):
+    '''
+    grid_data: [batch, height, width]
+    sample_grids: [batch, height, width, 2]    
+    '''
+    batch_size, height, width = (grid_data.shape[:])
+    sample_coords = tf.reshape(sample_grids, [batch_size,-1,2])
+    # pad to replicate the boundaries 1-ceiling, 2-floor
+    sample_coords = tf.stack([tf.clip_by_value(sample_coords[...,0],0,height-1),
+                            tf.clip_by_value(sample_coords[...,1],0,width-1)], axis=2)
+    i1 = tf.cast(tf.math.ceil(sample_coords[...,0]), dtype=tf.int32)
+    j1 = tf.cast(tf.math.ceil(sample_coords[...,1]), dtype=tf.int32)
+    i0 = tf.maximum(i1-1, 0)
+    j0 = tf.maximum(j1-1, 0)
+    # four data points q_ij
+    q00 = tf.gather_nd(grid_data, tf.stack([i0,j0],axis=2), batch_dims=1)
+    q01 = tf.gather_nd(grid_data, tf.stack([i0,j1],axis=2), batch_dims=1)
+    q11 = tf.gather_nd(grid_data, tf.stack([i1,j1],axis=2), batch_dims=1)
+    q10 = tf.gather_nd(grid_data, tf.stack([i1,j0],axis=2), batch_dims=1)    
+    # weights with normalised local coordinates
+    wi1 = sample_coords[...,0] - tf.cast(i0,dtype=tf.float32)
+    wi0 = 1 - wi1
+    wj1 = sample_coords[...,1] - tf.cast(j0,dtype=tf.float32)
+    wj0 = 1 - wj1
+    return tf.reshape(q00*wi0*wj0 + q01*wi0*wj1 + q11*wi1*wj1 + q10*wi1*wj0, [batch_size,height,width])
+
+
+def random_transform_generator(batch_size, corner_scale=.1):
+    # righ-multiplication affine
+    ori_corners = tf.tile([[[1.,1.], [1.,-1.], [-1.,1.], [-1.,-1.]]], [batch_size,1,1])
+    new_corners = ori_corners + tf.random.uniform([batch_size,4,2], -corner_scale, corner_scale)    
+    ori_corners = tf.concat([ori_corners,tf.ones([batch_size,4,1])], axis=2)
+    new_corners = tf.concat([new_corners,tf.ones([batch_size,4,1])], axis=2)
+    return tf.stack([tf.linalg.lstsq(ori_corners[n],new_corners[n]) for n in range(batch_size)], axis=0)
+
+
+@tf.function
+def random_image_transform(images):
+    # images: [batch_size, height, width]
+    reference_grid = get_reference_grid(images.shape[0:3])
+    random_transform = random_transform_generator(images.shape[0], corner_scale=0.1)
+    sample_grids = warp_grid(reference_grid, random_transform)
+    return resample_linear(images, sample_grids)
+
+
+class RegNet2D:
+
+    def __init__(self, minibatch_size, image_moving, image_fixed):
+
+        self.num_channel_initial = 32
+
+        self.minibatch_size = minibatch_size
+        self.grid_ref = get_reference_grid(image_fixed.shape.as_list()[1:3])
+        self.image_moving = image_moving
+        self.input_layer = tf.concat([self.image_moving, image_fixed], axis=3)
+
+        nc = [int(self.num_channel_initial*(2**i)) for i in range(5)]
+        h0, hc0 = downsample_resnet_block(self.input_layer, 2, nc[0], name='local_down_0')
+        h1, hc1 = downsample_resnet_block(h0, nc[0], nc[1], name='local_down_1')
+        h2, hc2 = downsample_resnet_block(h1, nc[1], nc[2], name='local_down_2')
+        h3, hc3 = downsample_resnet_block(h2, nc[2], nc[3], name='local_down_3')
+        h4 = conv_block(h3, nc[3], nc[4], name='local_deep_4')
+        hu3 = upsample_resnet_block(h4, hc3, nc[4], nc[3], name='local_up_3')
+        hu2 = upsample_resnet_block(hu3, hc2, nc[3], nc[2], name='local_up_2')
+        hu1 = upsample_resnet_block(hu2, hc1, nc[2], nc[1], name='local_up_1')
+        hu0 = upsample_resnet_block(hu1, hc0, nc[1], nc[0], name='local_up_0')
+
+        self.ddf = ddf_output(hu0, nc[0])
+        self.grid_warped = self.grid_ref + self.ddf
+
+    def warp_image(self, input_=None):
+        if input_ is None:
+            input_ = self.image_moving
+        return tf.contrib.resampler.resampler(input_, self.grid_warped)
---
+++ tutorials/registration/train_tf2.py
@@ -0,0 +1,91 @@
+# (Model) this uses TensorFlow
+
+import random
+import time
+import sys
+import os
+
+import tensorflow as tf
+import numpy as np
+
+import networks_tf2 as network
+import losses_tf2 as loss
+
+
+# 0 - read all the data
+data_dir = '../data/'
+images = np.load(data_dir+'images_train.npy')
+image_size = [images.shape[1], images.shape[2]]
+num_data = images.shape[0]
+
+
+# 1 - settings
+weight_regulariser = 1e-1
+parsed_input = sys.argv  # get idx at runtime
+if len(parsed_input) == 2:
+    weight_regulariser = float(parsed_input[1])
+
+minibatch_size = 8
+learning_rate = 1e-3
+total_iterations = int(50000+1)
+freq_info_print = 200
+freq_model_save = 5000
+
+model_dir_save = data_dir + 'arg_%s/' % weight_regulariser
+if not os.path.exists(model_dir_save):
+        os.makedirs(model_dir_save)
+model_file_save = model_dir_save + 'model_saved'
+
+
+# 2 - graph
+ph_moving_image = tf.placeholder(tf.float32, [minibatch_size]+image_size)
+ph_fixed_image = tf.placeholder(tf.float32, [minibatch_size]+image_size)
+input_moving_image = tf.expand_dims(ph_moving_image, axis=3)  # data augmentation here
+input_fixed_image = tf.expand_dims(ph_fixed_image, axis=3)  # data augmentation here
+
+# instantiate a network
+reg_net = networks.RegNet2D(minibatch_size=minibatch_size, image_moving=input_moving_image, image_fixed=input_fixed_image)
+
+# loss
+warped_moving_image = reg_net.warp_image(input_moving_image)  # warp the moving image with the predicted ddf
+
+loss_similarity = tf.reduce_mean(loss2.normalised_cross_correlation(warped_moving_image, input_fixed_image))
+# loss_similarity = tf.reduce_mean(loss2.sum_square_difference(warped_moving_image, input_fixed_image))
+loss_regularise = tf.reduce_mean(loss.bending_energy(reg_net.ddf))
+
+loss = loss_similarity + weight_regulariser*loss_regularise
+train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+
+
+# 3 - training
+num_minibatch = int(num_data/minibatch_size/2)
+train_indices = [i for i in range(num_data)]
+
+saver = tf.train.Saver(max_to_keep=1)
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+for step in range(total_iterations):
+
+    if step in range(0, total_iterations, num_minibatch):
+        random.shuffle(train_indices)
+
+    minibatch_idx = step % num_minibatch
+    # random pairs
+    indices_moving = train_indices[minibatch_idx*minibatch_size:(minibatch_idx+1)*minibatch_size]
+    indices_fixed = train_indices[::-1][minibatch_idx*minibatch_size:(minibatch_idx+1)*minibatch_size]
+    trainFeed = {ph_moving_image: images[indices_moving, ...], ph_fixed_image: images[indices_fixed, ...]}
+
+    sess.run(train_op, feed_dict=trainFeed)
+
+    if step in range(0, total_iterations, freq_info_print):
+        current_time = time.asctime(time.gmtime())
+        loss_train, loss_sim_train, loss_reg_train = sess.run(
+            [loss, loss_similarity, loss_regularise], feed_dict=trainFeed)
+
+        print('Step %d [%s]: Loss=%f (similarity=%f, regulariser=%f)' %
+              (step, current_time, loss_train, loss_sim_train, loss_reg_train))
+        print('  Moving-fixed image pair indices: %s - %s' % (indices_moving, indices_fixed))
+
+    if step in range(0, total_iterations, freq_model_save):
+        save_path = saver.save(sess, model_file_save, write_meta_graph=False)
+        print("Model saved in: %s" % save_path)
