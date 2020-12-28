f3f50315d2e1a71731b7f39641b3cacfa61346f6
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2020-12-28 11:39:15 +0000

    ref #6 changed to single utility function

--- tutorials/registration/tf_utils.py
+++ tutorials/registration/tf_utils.py
@@ -3,6 +3,7 @@
 import tensorflow as tf
 
 
+### network and layers
 def var_conv_kernel(ch_in, ch_out, name='W', initialiser=None):
     with tf.variable_scope(name):
         k_conv = [3, 3]
@@ -61,6 +62,38 @@ def ddf_output(input_, ch_in, name='ddf_summand'):
         return tf.nn.conv2d(input_, w, strides1, "SAME") + b
 
 
+class RegNet2D:
+
+    def __init__(self, minibatch_size, image_moving, image_fixed):
+
+        self.num_channel_initial = 32
+
+        self.minibatch_size = minibatch_size
+        self.grid_ref = get_reference_grid(image_fixed.shape.as_list()[1:3])
+        self.image_moving = image_moving
+        self.input_layer = tf.concat([self.image_moving, image_fixed], axis=3)
+
+        nc = [int(self.num_channel_initial*(2**i)) for i in range(5)]
+        h0, hc0 = downsample_resnet_block(self.input_layer, 2, nc[0], name='local_down_0')
+        h1, hc1 = downsample_resnet_block(h0, nc[0], nc[1], name='local_down_1')
+        h2, hc2 = downsample_resnet_block(h1, nc[1], nc[2], name='local_down_2')
+        h3, hc3 = downsample_resnet_block(h2, nc[2], nc[3], name='local_down_3')
+        h4 = conv_block(h3, nc[3], nc[4], name='local_deep_4')
+        hu3 = upsample_resnet_block(h4, hc3, nc[4], nc[3], name='local_up_3')
+        hu2 = upsample_resnet_block(hu3, hc2, nc[3], nc[2], name='local_up_2')
+        hu1 = upsample_resnet_block(hu2, hc1, nc[2], nc[1], name='local_up_1')
+        hu0 = upsample_resnet_block(hu1, hc0, nc[1], nc[0], name='local_up_0')
+
+        self.ddf = ddf_output(hu0, nc[0])
+        self.grid_warped = self.grid_ref + self.ddf
+
+    def warp_image(self, input_=None):
+        if input_ is None:
+            input_ = self.image_moving
+        return bilinear_resampler(input_, self.grid_warped)
+
+
+### transformation
 def get_reference_grid(grid_size):
     # grid_size: [batch_size, height, width]
     grid = tf.cast(tf.stack(tf.meshgrid(
@@ -108,7 +141,7 @@ def bilinear_resampler(grid_data, sample_grids):
 
 
 def random_transform_generator(batch_size, corner_scale=.1):
-    # righ-multiplication affine
+    # right-multiplication affine
     ori_corners = tf.tile([[[1.,1.], [1.,-1.], [-1.,1.], [-1.,-1.]]], [batch_size,1,1])
     new_corners = ori_corners + tf.random.uniform([batch_size,4,2], -corner_scale, corner_scale)    
     ori_corners = tf.concat([ori_corners,tf.ones([batch_size,4,1])], axis=2)
@@ -116,41 +149,61 @@ def random_transform_generator(batch_size, corner_scale=.1):
     return tf.stack([tf.linalg.lstsq(ori_corners[n],new_corners[n]) for n in range(batch_size)], axis=0)
 
 
-@tf.function
 def random_image_transform(images):
     # images: [batch_size, height, width]
     reference_grid = get_reference_grid(images.shape[0:3])
     random_transform = random_transform_generator(images.shape[0], corner_scale=0.1)
     sample_grids = warp_grid(reference_grid, random_transform)
-    return resample_linear(images, sample_grids)
+    return bilinear_resampler(images, sample_grids)
 
 
-class RegNet2D:
+### loss functions
+def normalised_cross_correlation(ts, ps, eps=0.0):
+    dp = ps - tf.reduce_mean(ps, axis=[1, 2, 3])
+    dt = ts - tf.reduce_mean(ts, axis=[1, 2, 3])
+    vp = tf.reduce_sum(tf.square(dp), axis=[1, 2, 3])
+    vt = tf.reduce_sum(tf.square(dt), axis=[1, 2, 3])
+    return tf.constant(1.0) - tf.reduce_sum(dp*dt / (tf.sqrt(vp*vt) + eps), axis=[1, 2, 3])
 
-    def __init__(self, minibatch_size, image_moving, image_fixed):
 
-        self.num_channel_initial = 32
+def normalised_cross_correlation2(ts, ps, eps=1e-6):
+    mean_t = tf.reduce_mean(ts, axis=[1, 2, 3])
+    mean_p = tf.reduce_mean(ps, axis=[1, 2, 3])
+    std_t = tf.reduce_sum(tf.sqrt(tf.square(mean_t)-tf.reduce_mean(tf.square(ts), axis=[1, 2, 3])), axis=[1, 2, 3])
+    std_p = tf.reduce_sum(tf.sqrt(tf.square(mean_p)-tf.reduce_mean(tf.square(ps), axis=[1, 2, 3])), axis=[1, 2, 3])
+    return -tf.reduce_mean((ts-mean_t)*(ps-mean_p) / (std_t*std_p+eps), axis=[1, 2, 3])
 
-        self.minibatch_size = minibatch_size
-        self.grid_ref = get_reference_grid(image_fixed.shape.as_list()[1:3])
-        self.image_moving = image_moving
-        self.input_layer = tf.concat([self.image_moving, image_fixed], axis=3)
 
-        nc = [int(self.num_channel_initial*(2**i)) for i in range(5)]
-        h0, hc0 = downsample_resnet_block(self.input_layer, 2, nc[0], name='local_down_0')
-        h1, hc1 = downsample_resnet_block(h0, nc[0], nc[1], name='local_down_1')
-        h2, hc2 = downsample_resnet_block(h1, nc[1], nc[2], name='local_down_2')
-        h3, hc3 = downsample_resnet_block(h2, nc[2], nc[3], name='local_down_3')
-        h4 = conv_block(h3, nc[3], nc[4], name='local_deep_4')
-        hu3 = upsample_resnet_block(h4, hc3, nc[4], nc[3], name='local_up_3')
-        hu2 = upsample_resnet_block(hu3, hc2, nc[3], nc[2], name='local_up_2')
-        hu1 = upsample_resnet_block(hu2, hc1, nc[2], nc[1], name='local_up_1')
-        hu0 = upsample_resnet_block(hu1, hc0, nc[1], nc[0], name='local_up_0')
+def sum_square_difference(i1, i2):
+    return tf.reduce_mean(tf.square(i1 - i2), axis=[1, 2, 3])  # use mean for normalised regulariser weighting
 
-        self.ddf = ddf_output(hu0, nc[0])
-        self.grid_warped = self.grid_ref + self.ddf
 
-    def warp_image(self, input_=None):
-        if input_ is None:
-            input_ = self.image_moving
-        return bilinear_resampler(input_, self.grid_warped)
+def gradient_dx(fv):
+    return (fv[:, 2:, 1:-1] - fv[:, :-2, 1:-1]) / 2
+
+
+def gradient_dy(fv):
+    return (fv[:, 1:-1, 2:] - fv[:, 1:-1, :-2]) / 2
+
+
+def gradient_txy(txy, fn):
+    return tf.stack([fn(txy[..., i]) for i in [0, 1]], axis=3)
+
+
+def gradient_norm(displacement, flag_l1=False):
+    dtdx = gradient_txy(displacement, gradient_dx)
+    dtdy = gradient_txy(displacement, gradient_dy)
+    if flag_l1:
+        norms = tf.abs(dtdx) + tf.abs(dtdy)
+    else:
+        norms = dtdx**2 + dtdy**2
+    return tf.reduce_mean(norms, [1, 2, 3])
+
+
+def bending_energy(displacement):
+    dtdx = gradient_txy(displacement, gradient_dx)
+    dtdy = gradient_txy(displacement, gradient_dy)
+    dtdxx = gradient_txy(dtdx, gradient_dx)
+    dtdyy = gradient_txy(dtdy, gradient_dy)
+    dtdxy = gradient_txy(dtdx, gradient_dy)
+    return tf.reduce_mean(dtdxx**2 + dtdyy**2 + 2*dtdxy**2, [1, 2, 3])
