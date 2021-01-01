8ee568143f4a51ce0558e9d61d5a879dd16b9866
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2021-01-01 14:42:21 +0000

    ref #6 listdir sorting bug fixed

--- tutorials/registration/np_utils.py
+++ tutorials/registration/np_utils.py
@@ -9,10 +9,10 @@ def get_image_arrays():
     PATH_TO_TRAIN = 'data/datasets-hn2dct/train'
     PATH_TO_TEST = 'data/datasets-hn2dct/test'
 
-    images = np.stack([mpimg.imread(os.path.join(PATH_TO_TRAIN, f)) for f in os.listdir(PATH_TO_TRAIN) if f.endswith('.png')],axis=0)  # stack at dim=0 consistent with tf
+    images = np.stack([mpimg.imread(os.path.join(PATH_TO_TRAIN, f)) for f in sorted(os.listdir(PATH_TO_TRAIN)) if f.endswith('.png')],axis=0)  # stack at dim=0 consistent with tf
     images = np.pad(images, [(0,0),(0,0),(0,1)])  # padding for an easier image size
 
-    test_images = np.stack([mpimg.imread(os.path.join(PATH_TO_TEST, f)) for f in os.listdir(PATH_TO_TEST) if (f.find('_')==-1 and f.endswith('.png'))],axis=0) 
+    test_images = np.stack([mpimg.imread(os.path.join(PATH_TO_TEST, f)) for f in sorted(os.listdir(PATH_TO_TEST)) if (f.find('_')==-1 and f.endswith('.png'))],axis=0) 
     test_images = np.pad(test_images, [(0,0),(0,0),(0,1)])  # padding for an easier image size
     test_indices = [[0,0,1,1,2,2],[1,2,0,2,0,1]] # [moving,fixed]
 
