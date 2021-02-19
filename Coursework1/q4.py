d1bedcc0de619e65417b2d2dba3b1ec8470ad388
David Scobie <david.scobie.20@ucl.ac.uk>
2021-02-19 16:05:42 +0000

    no improvement in R2 with more iterations

--- Coursework1/q4.py
+++ Coursework1/q4.py
@@ -5,16 +5,51 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.metrics import mean_squared_error
 from numpy.linalg import inv
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.metrics import r2_score
 
 #a
-
 
 traindata = pd.read_csv('ADNI_CW1_TRAIN.csv')
 testdata = pd.read_csv('ADNI_CW1_TEST.csv')
 
 #seperate y and x into separate dataframes
 response = traindata['BRAAK34_SUVR']
+norm_resp = np.round(response/np.max(response),0)
 
 column_headers = list(traindata)
-inputs_list = column_headers[4:]
+inputs_list = column_headers[5:]
 features = traindata[inputs_list]
+
+#normailse training data
+scale_feat = pd.DataFrame()
+for feature in inputs_list:
+    scaler = StandardScaler()
+    scaler = scaler.fit(features[[feature]]).transform(features[[feature]])
+    scale_feat[feature] = scaler.reshape(1,len(response))[0]
+
+print(scale_feat)
+print(response)
+
+reg = LogisticRegression(penalty='elasticnet',solver='saga',C=1,l1_ratio=0.5).fit(scale_feat,norm_resp)
+print(reg.coef_)
+print(reg.intercept_)
+print(reg.n_iter_)
+print(np.max(np.round(response,0)))
+print(np.min(np.round(response,0)))
+
+R2_array=[]
+for i in range (8):
+    reg_CV = LogisticRegressionCV(penalty='elasticnet',solver='saga',scoring = 'balanced_accuracy', l1_ratios = [0.5], Cs =10, cv=2+i).fit(scale_feat,norm_resp)
+    # print(reg_CV.coef_)
+    # print(reg_CV.Cs_)
+    # print(reg_CV.C_)
+    # print(reg_CV.n_iter_)
+    prediction = reg_CV.predict(scale_feat)
+    print(prediction)
+    R2 = r2_score(norm_resp, prediction)
+    print(R2)
+    R2_array.append(R2)
+plt.plot(R2_array)
+plt.show()
