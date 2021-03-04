4ea24dfc219975679f7bedc7f9a12662551874ba
David Scobie <david.scobie.20@ucl.ac.uk>
2021-03-04 19:47:35 +0000

    planes for 1d

--- Coursework1/q1.py
+++ Coursework1/q1.py
@@ -57,10 +57,10 @@ c = (-2*np.log((std_Dementia_ABETA)/(std_CN_ABETA)))-((mean_Dementia_ABETA**2)/(
 x_bound = (-b-np.sqrt((b**2)-(4*a*c)))/(2*a)
 print(x_bound)
 
-# plt.figure(0)
-# plt.plot(x,fx_CN)
-# plt.plot(x,fx_Dementia)
-# plt.axvline(x=x_bound, ymin=0, ymax=1)
+plt.figure(0)
+plt.plot(x,fx_CN)
+plt.plot(x,fx_Dementia)
+plt.axvline(x=x_bound, ymin=0, ymax=1)
 
 #b
 
@@ -92,11 +92,10 @@ xmin, xmax = min(features['ABETA']),max(features['ABETA'])
 ymin, ymax = min(features['TAU']),max(features['TAU'])
 xd = np.array([xmin, xmax])
 yd = m*xd + c
-# plt.figure(1)
-# plt.plot(xd, yd, 'k', lw=1, ls='--')
+plt.figure(1)
+plt.plot(xd, yd, 'k', lw=1, ls='--')
 
-# plt.scatter(MCI_less['ABETA'],MCI_less['TAU'], c=response)
-# plt.show()
+plt.scatter(MCI_less['ABETA'],MCI_less['TAU'], c=response)
 
 #c
 
@@ -135,7 +134,7 @@ cov_inv = inv(covar)
 CN_Dementia_orth = np.matmul(cov_inv,np.transpose(CN_mu-Dementia_mu))
 CN_MCI_orth = np.matmul(cov_inv,np.transpose(CN_mu-MCI_mu))
 Dementia_MCI_orth = np.matmul(cov_inv,np.transpose(Dementia_mu-MCI_mu))
-print(CN_Dementia_orth)
+# print(CN_Dementia_orth)
 
 #finding midpoints
 CN_Dementia_mid = (CN_mu+Dementia_mu)/2
@@ -152,34 +151,34 @@ CN_MCI_c = CN_MCI_mid[0][1] - (CN_MCI_grad*CN_MCI_mid[0][0])
 Dementia_MCI_grad = Dementia_MCI_orth[0]/-Dementia_MCI_orth[1]
 Dementia_MCI_c = Dementia_MCI_mid[0][1] - (Dementia_MCI_grad*Dementia_MCI_mid[0][0])
 
-# print(CN_Dementia_c)
-# print(CN_MCI_c)
-# print(Dementia_MCI_c)
+print(CN_Dementia_c)
+print(CN_MCI_c)
+print(Dementia_MCI_c)
 
 xd = np.array([0, 10])
 
-# print(Dementia_MCI_grad)
-# print(CN_MCI_grad)
-# print(CN_Dementia_grad)
+print(Dementia_MCI_grad)
+print(CN_MCI_grad)
+print(CN_Dementia_grad)
 
 #equations of boundaries
 y_CN_Dementia = CN_Dementia_grad*xd + CN_Dementia_c
 y_CN_MCI = CN_MCI_grad*xd + CN_MCI_c
 y_Dementia_MCI = Dementia_MCI_grad*xd + Dementia_MCI_c
 
-# plt.figure(2)
+plt.figure(2)
 
-# plt.plot(xd, y_CN_Dementia, 'k', lw=1, ls='--')
-# plt.plot(xd, y_CN_MCI, 'k', lw=1, ls='--')
-# plt.plot(xd, y_Dementia_MCI, 'k', lw=1, ls='--')
+plt.plot(xd, y_CN_Dementia, 'k', lw=1, ls='--')
+plt.plot(xd, y_CN_MCI, 'k', lw=1, ls='--')
+plt.plot(xd, y_Dementia_MCI, 'k', lw=1, ls='--')
 
-# plt.scatter(CN_both_features[:,0],CN_both_features[:,1])
-# plt.scatter(Dementia_both_features[:,0],Dementia_both_features[:,1])
-# plt.scatter(MCI_both_features[:,0],MCI_both_features[:,1])
+plt.scatter(CN_both_features[:,0],CN_both_features[:,1])
+plt.scatter(Dementia_both_features[:,0],Dementia_both_features[:,1])
+plt.scatter(MCI_both_features[:,0],MCI_both_features[:,1])
 
 #d
-# print(np.max(traindata['ABETA']))
-# print(np.max(traindata['TAU']))
+print(np.max(traindata['ABETA']))
+print(np.max(traindata['TAU']))
 
 CN_ABETA_TAU = CN_abeta * CN_tau
 DEMENTIA_ABETA_TAU = Dementia_abeta * Dementia_tau
@@ -206,28 +205,14 @@ cov_inv = inv(covar)
 CN_Dementia_orth = np.matmul(cov_inv,np.transpose(CN_mu-Dementia_mu))
 CN_MCI_orth = np.matmul(cov_inv,np.transpose(CN_mu-MCI_mu))
 Dementia_MCI_orth = np.matmul(cov_inv,np.transpose(Dementia_mu-MCI_mu))
-print(CN_Dementia_orth)
+print(Dementia_MCI_orth)
 
 #finding midpoints
 CN_Dementia_mid = (CN_mu+Dementia_mu)/2
 CN_MCI_mid = (CN_mu+MCI_mu)/2
 Dementia_MCI_mid = (Dementia_mu+MCI_mu)/2
-print(CN_Dementia_mid)
 
-# fig = plt.figure(num=3, clear=True)
-# ax = fig.add_subplot(1, 1, 1, projection='3d')
-
-# x = np.array([[-12.4, -1.9], [4.4, -12.4]])
-# y = np.array([[1.7, 1.5], [-1.9, 1.7]])
-# z = np.array([[6.4, -1.7], [-12.4, -5]])
-
-# x = np.array([[-12.4, -1.9,4.4]])
-# y = np.array([[1.7, 1.5,-1.9]])
-# z = np.array([[6.4, -1.7,-12.4]])
-
-# ax.plot_surface(x, y, z)
-# ax.set(xlabel='x', ylabel='y', zlabel='z')
-
+#plotting CN Dementia
 points = [[-12.4, 1.7, 6.4],
            [-1.9, 1.5, -1.7],
            [4.4, -1.9, -12.4]]
@@ -251,14 +236,14 @@ xx, yy = np.meshgrid(range(10), range(10))
 
 z = (-normal[0] * xx - normal[1] * yy - d) * 1. / normal[2]
 
-# plot the surface
 plt3d = plt.figure().gca(projection='3d')
 plt3d.plot_surface(xx, yy, z)
-plt3d.set(xlabel='x', ylabel='y', zlabel='z')
+plt3d.set(xlabel='ABETA', ylabel='TAU', zlabel='ABETA*TAU')
 
-points = [[-17, 7, 4],
-           [-1.9, 1.5, -1.7],
-           [4.4, -1.9, -12.4]]
+#plotting CN MCI
+points = [[-2.6, 0.1, 4],
+           [-0.3, 1.5, -0.1],
+           [4.4, -0.3, -2.6]]
 
 p0, p1, p2 = points
 x0, y0, z0 = p0
@@ -279,9 +264,38 @@ xx, yy = np.meshgrid(range(10), range(10))
 
 z = (-normal[0] * xx - normal[1] * yy - d) * 1. / normal[2]
 
-# plot the surface
 plt3d.plot_surface(xx, yy, z)
 
+#plotting Dementia MCI
+points = [[9.8, -1.6, 4],
+           [1.6, 1.5, 1.6],
+           [4.4, 1.6, 9.8]]
+
+p0, p1, p2 = points
+x0, y0, z0 = p0
+x1, y1, z1 = p1
+x2, y2, z2 = p2
+
+ux, uy, uz = u = [x1-x0, y1-y0, z1-z0]
+vx, vy, vz = v = [x2-x0, y2-y0, z2-z0]
+
+u_cross_v = [uy*vz-uz*vy, uz*vx-ux*vz, ux*vy-uy*vx]
+
+point  = np.array(p0)
+normal = np.array(u_cross_v)
+
+d = -point.dot(normal)
+
+xx, yy = np.meshgrid(range(10), range(10))
+
+z = (-normal[0] * xx - normal[1] * yy - d) * 1. / normal[2]
+
+plt3d.plot_surface(xx, yy, z)
+
+#plotting the underlying points
+plt3d.scatter(CN_3_features[:,0],CN_3_features[:,1],CN_3_features[:,2])
+plt3d.scatter(Dementia_3_features[:,0],Dementia_3_features[:,1],Dementia_3_features[:,2])
+plt3d.scatter(MCI_3_features[:,0],MCI_3_features[:,1],MCI_3_features[:,2])
 
 plt.show()
 
