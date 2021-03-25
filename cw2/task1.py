332a6f9df952bd401b4d471666e79b3f079fbed5
David Scobie <david.scobie.20@ucl.ac.uk>
2021-03-25 16:41:04 +0000

    reading in a loop of frames in my_data_generator

--- cw2/task1.py
+++ cw2/task1.py
@@ -11,11 +11,29 @@ num_frames_per_subject = 1 #In reality it is average of 27 per person
 # dataset = '/subject%06d_frame%08d' % (iSbj, idx_frame)
 # frame = tf.transpose(tf.keras.utils.HDF5Matrix(filename, dataset)) / 255
 
-frame1 = tf.transpose(tf.keras.utils.HDF5Matrix(filename, 'frame_0004_003' ))
+frame1 = tf.transpose(tf.keras.utils.HDF5Matrix(filename, 'frame_0004_003' )) / 255
 print(frame1)
-print(tf.math.reduce_max(frame1))
+# print(tf.math.reduce_max(frame1))
 
-## data loader using a generator
+##MY DATA GENERATOR
+def my_data_generator(subject_indices):
+    for iSbj in subject_indices:
+        idx_frame = 3
+        dataset = 'frame_%04d_%03d' % (iSbj, idx_frame)
+        print(dataset)
+        # print('hi')
+        frame = tf.transpose(tf.keras.utils.HDF5Matrix(filename, dataset)) / 255
+        print(frame)
+        # # yield (tf.expand_dims(frame, axis=2), label)
+        # yield frame
+
+subject_indicies = np.array([4,6,77])
+training_dataset = my_data_generator(subject_indicies)
+print(training_dataset)
+
+
+
+## DATA GENERATOR FROM CLASSIFICATION (h5 FILES)
 # num_subjects = tf.keras.utils.HDF5Matrix(filename, '/num_subjects').data.value[0][0]
 # validation_split = 0.2
 # num_training = int(tf.math.floor(num_subjects*(1-validation_split)).numpy())
@@ -23,21 +41,19 @@ print(tf.math.reduce_max(frame1))
 # training_indices = range(num_training)
 # validation_indices = range(num_training,num_subjects)
 # num_frames_per_subject = 1
-def data_generator(subject_indices):
-    for iSbj in subject_indices:
-        dataset = '/subject%06d_num_frames' % iSbj
-        num_frames = tf.keras.utils.HDF5Matrix(filename, dataset)[0][0]
-        idx_frame = random.sample(range(num_frames),num_frames_per_subject)[0]
-        dataset = '/subject%06d_frame%08d' % (iSbj, idx_frame)
-        frame = tf.transpose(tf.keras.utils.HDF5Matrix(filename, dataset)) / 255
-        dataset = '/subject%06d_label%08d' % (iSbj, idx_frame)
-        label = tf.keras.utils.HDF5Matrix(filename, dataset)[0][0]
-        yield (tf.expand_dims(frame, axis=2), label)
+# def data_generator(subject_indices):
+#     for iSbj in subject_indices:
+#         dataset = '/subject%06d_num_frames' % iSbj
+#         num_frames = tf.keras.utils.HDF5Matrix(filename, dataset)[0][0]
+#         idx_frame = random.sample(range(num_frames),num_frames_per_subject)[0]
+#         dataset = '/subject%06d_frame%08d' % (iSbj, idx_frame)
+#         frame = tf.transpose(tf.keras.utils.HDF5Matrix(filename, dataset)) / 255
+#         dataset = '/subject%06d_label%08d' % (iSbj, idx_frame)
+#         label = tf.keras.utils.HDF5Matrix(filename, dataset)[0][0]
+#         yield (tf.expand_dims(frame, axis=2), label)
 
 
-
-
-
+##SEGMENTATION NPY FILE DATA READER
 # path_to_data = './data/dataset70-200'
 # path_to_save = './result'
 
