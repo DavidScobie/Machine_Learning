ae0a4b0e0d2a69a9f39ee1ff6b48fe1f59c2c7b7
David Scobie <david.scobie.20@ucl.ac.uk>
2021-04-24 06:50:21 +0100

    prediction mask gets saved to txt file in pred_masks folder and image it in read_txt_files.py

--- cw2/T1/T1_small_sample.py
+++ cw2/T1/T1_small_sample.py
@@ -186,7 +186,7 @@ model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5),
               metrics=['MeanAbsoluteError'])
 
 #don't bother with shuffling and batches for now
-history_callback = model.fit(training_batch, epochs=int(3),validation_data = validation_batch)
+history_callback = model.fit(training_batch, epochs=int(1),validation_data = validation_batch)
 print('Training done.')
 
 #try a frame to test the model
@@ -209,6 +209,7 @@ for i in range (test_pred_shape[0]):
 
 plt.figure(2)
 plt.imshow(tf.image.convert_image_dtype(test_pred_mask, tf.int32))
+np.savetxt(os.path.join('pred_masks', 'pred_mask.txt'), tf.image.convert_image_dtype(test_pred_mask, tf.int32).numpy())
 
 #Image the corresponding frame and label
 test_label_0 = tf.math.divide(tf.keras.utils.HDF5Matrix(filename, 'label_0060_000_00' ),255)
@@ -238,4 +239,4 @@ val_loss_history = history_callback.history["val_loss"]
 numpy_val_loss_history = np.array(val_loss_history)
 np.savetxt("val_loss_history.txt", numpy_val_loss_history, delimiter=",")
 
-plt.show()
+# plt.show()
---
+++ cw2/read_txt_files.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+epo=300
+
+loss_fname = './cluster_loss/epo300_200subj_LR_1e-5_l_h.txt'
+numpy_loss_history = np.loadtxt(loss_fname)
+
+val_loss_fname = './cluster_loss/epo300_200subj_LR_1e-5_v_l_h.txt'
+numpy_val_loss_history = np.loadtxt(val_loss_fname)
+
+pred_mask_fname = './T1/pred_masks/pred_mask.txt'
+numpy_p_m = np.loadtxt(pred_mask_fname)
+
+plt.figure(0)
+plt.plot(numpy_val_loss_history,numpy_loss_history, 'bo')
+plt.xlabel('validation loss')
+plt.ylabel('training loss')
+
+plt.figure(1)
+plt.plot(np.linspace(1,epo,num=epo),numpy_loss_history)
+plt.ylabel('loss')
+plt.xlabel('epoch')
+
+plt.figure(2)
+plt.plot(np.linspace(1,epo,num=epo),numpy_val_loss_history)
+plt.ylabel('validation loss')
+plt.xlabel('epoch')
+
+plt.figure(3)
+plt.imshow(numpy_p_m)
+plt.show()
