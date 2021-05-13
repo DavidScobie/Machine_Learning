4a7177e350a25e1d3300bf5030620b405916b783
David Scobie <david.scobie.20@ucl.ac.uk>
2021-05-13 14:51:43 +0100

    accuracy score for 10 test frames of T5_random_frame

--- cw2/T5/T5_bland_altman.py
+++ cw2/T5/T5_bland_altman.py
@@ -37,10 +37,47 @@ keys = f.keys()
 filename = './data/dataset70-200.h5'
 frame_size = [58,52,1]
 
-biggun = tf.Variable(tf.zeros([10,58,52], tf.float32))
-for i in range (10):
-    test_indices = range(189+i,190+i)
+#initialise arrays
+stck_test_preds = tf.Variable(tf.zeros([10,58,52], tf.float32))
 
+stc_t_p_arr = np.zeros([10,58,52])
+pred_array = np.zeros([10,3016,1])
+# mean = np.zeros([10,3016,1])
+# diff = np.zeros([10,3016,1])
+numer = np.zeros([10])
+denom = np.zeros([10])
+DICE = np.zeros([10])
+eps = 1e-6
+
+for i in range (10):
+    #first of all getting the true label
+
+        #majority voting
+    l0_dataset = 'label_%04d_%03d_00' % (189+i, 4)
+    l1_dataset = 'label_%04d_%03d_01' % (189+i, 4)
+    l2_dataset = 'label_%04d_%03d_02' % (189+i, 4)
+
+    label0 = tf.cast(tf.keras.utils.HDF5Matrix(filename, l0_dataset),dtype=tf.float32)
+    label1 = tf.cast(tf.keras.utils.HDF5Matrix(filename, l1_dataset),dtype=tf.float32)
+    label2 = tf.cast(tf.keras.utils.HDF5Matrix(filename, l2_dataset),dtype=tf.float32)
+
+    sum_of_labs = label0+label1+label2
+
+    sum_of_labs_shape = sum_of_labs.shape
+    maj_label = tf.Variable(tf.zeros([sum_of_labs_shape[0],sum_of_labs_shape[1]], tf.int32))
+    for k in range (sum_of_labs_shape[0]):
+        for j in range (sum_of_labs_shape[1]):
+            if sum_of_labs[k][j] >= 2:
+                maj_label[k,j].assign(1)
+    print(tf.convert_to_tensor(maj_label))
+    # squeezed = tf.squeeze(tf.image.convert_image_dtype(true_frame, tf.float32))
+    numpy = tf.make_ndarray(tf.make_tensor_proto(tf.convert_to_tensor(maj_label)))
+    true_array = np.reshape(numpy,(3016,1))
+
+
+    #now working with model predictions
+    test_indices = range(189+i,190+i)
+    idx_frame_indics= range(4,5)
     def my_test_generator(subject_indices):
         for iSbj in subject_indices:
             # idx_frame_indics = range(num_subjects)
@@ -59,23 +96,37 @@ for i in range (10):
     test_batch = test_dataset.shuffle(buffer_size=1024).batch(1)
 
     y_pred = model.predict(test_batch)
-    print(y_pred)
 
-    biggun[i,:,:].assign(tf.squeeze(tf.image.convert_image_dtype(y_pred, tf.float32)))
+    stck_test_preds[i,:,:].assign(tf.squeeze(tf.image.convert_image_dtype(y_pred, tf.float32)))
 
+    # plt.figure(i+1)
+    # plt.imshow(stck_test_preds[i,:,:])
 
-print(biggun)
+    stc_t_p_arr[i,:,:] = (tf.make_ndarray(tf.make_tensor_proto(stck_test_preds[i,:,:])))
 
-for i in range (10):
-    plt.figure(i+1)
-    plt.imshow(biggun[i,:,:])
+    pred_array[i,:,:] = (np.reshape(stc_t_p_arr[i,:,:],(3016,1)))
 
+    numer[i] = 2*np.sum(true_array*pred_array[i,:,:],axis = 0)
+    denom[i] = eps + np.sum(pred_array[i,:,:],axis = 0) + np.sum(true_array,axis = 0)
+    DICE[i] = 1 - numer[i]/denom[i]
+    
+    
+    # plt.figure(i+11)
+    # plt.imshow(numpy)
+print(DICE)
+acc = 1-DICE
+print(acc)
+    # print(pred_array[i,:,:])
+    # print(true_array)
 
+    # mean[i,:,:] = (pred_array[i,:,:] + true_array)/2
+    # diff[i,:,:] = (true_array - pred_array[i,:,:])
+    # print(mean)
+    # print(diff)
 
-# pred_array = np.reshape(test_pred,(3016,1))
+    # #reshape into column vectors
+    # mean = np.reshape(mean,[10*3016])
 
-# mean = (pred_array + true_array)/2 
-# diff = true_array - pred_array
 
 # plt.figure(2)
 # plt.plot(mean,diff, 'bo')
